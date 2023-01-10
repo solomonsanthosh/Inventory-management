585e8281868b85cd13370870b8f8b1fccd9fb793
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-01-10 20:58:39 +0530

    commit

--- src/App.js
+++
@@ -1,9 +0,0 @@
-
-function App() {
-  return (
-    <div className="h-screen bg-blue-100">
-    </div>
-  );
-}
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'bgWhite': '#F0F2F4',
+      'white': '#FFFFFF',
+      'highlight': '#10AB71',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
