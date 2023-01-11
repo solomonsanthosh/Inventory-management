8b16b5bc4530a1819e57c34328c9ef4e69a3f0d5
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-01-11 19:48:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Manager from "./Pages/Manager/Manager";
 import Suggest from "./Pages/Manager/Suggest";
 import EmployeeForm from "./Pages/EmployeeForm";
+import SignIn from "./Pages/auth/SignIn";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
+					<Route path="/signin" element={<SignIn />}></Route>
 					<Route path="/manager" element={<Manager />}></Route>
 					<Route path="/form" element={<EmployeeForm />}></Route>
 					<Route
