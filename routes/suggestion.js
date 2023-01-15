c42a9046d8943735d1293fc962eb6ff700a2db00
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-01-16 00:10:47 +0530

    email feature added

--- routes/suggestion.js
+++ routes/suggestion.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 
+const sendEmail = require("../controllers/sendEmail");
 const {
-	getSuggestions, postSuggestion,
+  getSuggestions,
+  postSuggestion,
 } = require("../controllers/suggestion.controller");
 
 router.get("/getsuggestions/:partno", getSuggestions);
 router.post("/postsuggestion", postSuggestion);
+router.post("/sendemail", sendEmail);
 module.exports = router;
