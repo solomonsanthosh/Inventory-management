5395389f8b1f3e095ec9b1792bc2fd02b8376b89
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-01-16 00:11:07 +0530

    email feature added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Manager from "./Pages/Manager/Manager";
 import Suggest from "./Pages/Manager/Suggest";
@@ -7,50 +7,41 @@ import CreateAccount from "./Pages/Manager/CreateAccount";
 import ShowAccounts from "./Pages/Manager/ShowAccounts";
 import TicketGenerate from "./Pages/Ticket/TicketGenerate";
 import TicketDashboard from "./Pages/Ticket/TicketDashboard";
-import {useState, useEffect} from "react";
-import {checkLogin} from "./Axios/user";
+import { useState, useEffect } from "react";
+import { checkLogin } from "./Axios/user";
 
 function App() {
-	const [user, setUser] = useState(false);
-	// useEffect(() => {
-	// 	if (localStorage.getItem("jwt")) {
-	// 		checkLogin(localStorage.getItem("jwt")).then((res) => {
-	// 			if (res?.status == 200) {
-	// 				setUser(true);
-	// 			}
-	// 		});
-	// 	}
-	// }, [user]);
+  const [user, setUser] = useState(false);
+  // useEffect(() => {
+  // 	if (localStorage.getItem("jwt")) {
+  // 		checkLogin(localStorage.getItem("jwt")).then((res) => {
+  // 			if (res?.status == 200) {
+  // 				setUser(true);
+  // 			}
+  // 		});
+  // 	}
+  // }, [user]);
 
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					{/* {user ? ( */}
-						<Route
-							path="/ticketform"
-							element={<TicketGenerate />}
-						></Route>
-					{/* ) : null} */}
-					<Route
-						path="/ticketdashboard"
-						element={<TicketDashboard />}
-					></Route>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* {user ? ( */}
+          <Route path="/ticketform" element={<TicketGenerate />}></Route>
+          {/* ) : null} */}
+          <Route path="/ticketdashboard" element={<TicketDashboard />}></Route>
 
-					<Route path="/" element={<SignIn />}></Route>
-					<Route path="/manager" element={<Manager />}></Route>
-					<Route
-						path="/manager/showaccounts"
-						element={<ShowAccounts />}
-					></Route>
-					<Route
-						path="/manager/suggest"
-						element={<Suggest />}
-					></Route>
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+          <Route path="/" element={<SignIn />}></Route>
+          <Route path="/manager" element={<Manager />}></Route>
+          <Route
+            path="/manager/showaccounts"
+            element={<ShowAccounts />}
+          ></Route>
+          <Route path="/manager/suggest" element={<Suggest />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/Axios/manager.js
+++ src/Axios/manager.js
@@ -1,16 +1,24 @@
 import axios from "axios";
 
 export const createAccountAxios = async (user) => {
-	return await axios.post(`${process.env.REACT_APP_BASE_URL}/create`, {
-		user: user,
-	});
+  return await axios.post(`${process.env.REACT_APP_BASE_URL}/create`, {
+    user: user,
+  });
 };
 
 export const getAccounts = async () => {
-	return await axios.get(`${process.env.REACT_APP_BASE_URL}/show`);
+  return await axios.get(`${process.env.REACT_APP_BASE_URL}/show`);
 };
 
-
 export const getSuggestion = async (partno) => {
-	return await axios.get(`${process.env.REACT_APP_BASE_URL}/getsuggestions/${partno}`);
+  return await axios.get(
+    `${process.env.REACT_APP_BASE_URL}/getsuggestions/${partno}`
+  );
+};
+
+export const sendEmail = async (email, product) => {
+  return await axios.post(`${process.env.REACT_APP_BASE_URL}/sendemail`, {
+    email: email,
+    product: product,
+  });
 };
