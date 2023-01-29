2ab3e0e6199c6812ceccb7bc02b5a11217749afc
Prasanna <prasannavelmurugan0200@gmail.com>
2023-01-29 22:26:01 +0530

    rv

--- src/Axios/ticket.js
+++ src/Axios/ticket.js
@@ -4,14 +4,11 @@ export const gettickets = async () => {
 	return await axios.get(`${process.env.REACT_APP_BASE_URL}/gettickets`);
 };
 
-
 export const getticketSingle = async (id) => {
 	return await axios.get(
 		`${process.env.REACT_APP_BASE_URL}/gettickets/${id}`
 	);
 };
-
-
 
 export const suggesttickets = async () => {
 	return await axios.get(
--- src/Pages/Ticket/TicketGenerate.jsx
+++ src/Pages/Ticket/TicketGenerate.jsx
@@ -1,25 +1,34 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Navigate, useNavigate} from "react-router-dom";
-import {ticketgenerate} from "../../Axios/ticket";
+import {ticketgenerate, gettickets} from "../../Axios/ticket";
+import {getProducts} from "../../Axios/store";
 import SideNav from "../../components/SideNav/SideNav";
 const TicketGenerate = () => {
 	const navigate = useNavigate();
 
 	const [part, setPart] = useState("");
 	const [quantity, setQuantity] = useState();
+	const [allproducts, setAllProducts] = useState([]);
 
 	const generateTicket = () => {
 		if (localStorage.getItem("name")) {
-			// ticketgenerate(part, quantity, localStorage.getItem("name"))
+			ticketgenerate(part, quantity, localStorage.getItem("name"));
 			navigate("/ticketdashboard");
 		} else {
 			console.log("error");
 		}
 	};
 
+	useEffect(() => {
+		getProducts().then((response) => {
+			setAllProducts(response.data);
+		});
+	});
+
 	return (
 		<div className="w-full flex flex-col justify-center items-center min-h-screen bg-[#F5F5F5]">
 			<SideNav />
+
 			<div className="w-full flex flex-col justify-center items-center max-w-xs">
 				<form className="w-full bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 					<div className="mb-4">
@@ -36,6 +45,27 @@ const TicketGenerate = () => {
 							placeHolder="1xefhg2345"
 							onChange={(e) => setPart(e.target.value)}
 						/>
+					</div>
+					<div className="mb-4">
+						<label
+							className="block text-gray-700 text-sm font-bold mb-2"
+							htmlFor="productPartNo"
+						>
+							Product Part No
+						</label>
+						<select
+							name=""
+							id=""
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						>
+							{allproducts.map((part_no) => {
+								return (
+									<option value="part1">
+										{part_no.product_part_no}
+									</option>
+								);
+							})}
+						</select>
 					</div>
 					<div className="mb-6">
 						<label
