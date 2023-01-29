dd0fb3bd6f06733d32c42693d3b920d6a4d5cd9b
solomonsanthosh <solosanty2064@gmail.com>
2023-01-29 19:00:55 +0530

    SS

--- src/Pages/Manager/LocalStorage.jsx
+++ src/Pages/Manager/LocalStorage.jsx
@@ -1,69 +1,67 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import SideNav from "./SideNav/SideNav";
 import "./manager.css";
-import {getticketSingle, postStore} from "../../Axios/ticket";
+import { getticketSingle, postStore } from "../../Axios/ticket";
 function Manager() {
-	const [tickets, setTickets] = useState([]);
-	useEffect(() => {
-		getticketSingle("local").then((res) => {
-			setTickets(res.data);
-			console.log("====================================");
-			console.log(res.data);
-			console.log("====================================");
-		});
-	}, []);
+  const [tickets, setTickets] = useState([]);
+  useEffect(() => {
+    getticketSingle("local").then((res) => {
+      setTickets(res.data);
+      console.log("====================================");
+      console.log(res.data);
+      console.log("====================================");
+    });
+  }, []);
 
-	return (
-		<div className="h-full min-h-screen relative bg-[#F5F5F5] ">
-			{/* <div className="floatingButton" onClick={() => {
-				postStore()
-			}}>
-				<img
-					src="https://img.icons8.com/material-two-tone/30/null/shipping-product.png"
-					alt=""
-				/>
-			</div> */}
-			<div className="w-full">
-				<div className="dash">
-					<h1 className="title">Tickets</h1>
+  return (
+    <div className="h-full min-h-screen relative bg-[#F5F5F5] ">
+      <div className="floatingButton">
+        <img
+          src="https://img.icons8.com/material-two-tone/30/null/shipping-product.png"
+          alt=""
+        />
+      </div>
+      <div className="w-full">
+        <div className="dash">
+          <h1 className="title">Tickets</h1>
 
-					<table>
-						<thead>
-							<th>Ticket ID</th>
-							<th>Part Name</th>
-							<th>Quantity</th>
-							<th>Status</th>
-						</thead>
-						<tbody>
-							{tickets?.map((ticket) => {
-								console.log(ticket);
+          <table>
+            <thead>
+              <th>Ticket ID</th>
+              <th>Part Name</th>
+              <th>Quantity</th>
+              <th>Status</th>
+            </thead>
+            <tbody>
+              {tickets?.map((ticket) => {
+                console.log(ticket);
 
-								return (
-									<tr>
-										<td>{ticket.ticket_id}</td>
+                return (
+                  <tr>
+                    <td>{ticket.ticket_id}</td>
 
-										<td>{ticket.product_part_no}</td>
-										<td>{ticket.product_quantity}</td>
-										<td>
-											{ticket.status == "OPEN" ? (
-												<div className="text-[#FF3131] font-bold p-2 rounded-md ">
-													{ticket.status}
-												</div>
-											) : (
-												<div className="text-[#4CBB17] font-bold p-2 rounded-md ">
-													{ticket.status}
-												</div>
-											)}
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	);
+                    <td>{ticket.product_part_no}</td>
+                    <td>{ticket.product_quantity}</td>
+                    <td>
+                      {ticket.status == "OPEN" ? (
+                        <div className="text-[#FF3131] font-bold p-2 rounded-md ">
+                          {ticket.status}
+                        </div>
+                      ) : (
+                        <div className="text-[#4CBB17] font-bold p-2 rounded-md ">
+                          {ticket.status}
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Manager;
--- src/Pages/Manager/Manager.jsx
+++ src/Pages/Manager/Manager.jsx
@@ -6,24 +6,22 @@ import { calculateRange, sliceData } from "../../utils/table-pagination";
 
 function Manager() {
   const [tickets, setTickets] = useState([]);
-  const [showTicket,setShowTicket] = useState([]);
+  const [showTicket, setShowTicket] = useState([]);
   const [pagination, setPagination] = useState([]);
   const [page, setPage] = useState(1);
   useEffect(() => {
     gettickets().then((res) => {
       setTickets(res.data);
       setPagination(calculateRange(res.data, 10));
-      
-      setShowTicket(sliceData(res.data, page, 10))
+
+      setShowTicket(sliceData(res.data, page, 10));
     });
   }, []);
   const __handleChangePage = (new_page) => {
-
     setPage(new_page);
     console.log(new_page);
-    
+
     setShowTicket(sliceData(tickets, new_page, 10));
-    
   };
   return (
     <div className="h-full min-h-screen  bg-[#F5F5F5] ">
@@ -40,49 +38,51 @@ function Manager() {
               <th>Status</th>
             </thead>
             <tbody>
-              
               {showTicket?.map((ticket) => {
-                
-
                 return (
                   <tr>
                     <td>{ticket.ticket_id}</td>
-                    
+
                     <td>{ticket.product_part_no}</td>
                     <td>{ticket.product_quantity}</td>
-                    <td >
-                      {ticket.status == 'OPEN' ?
-                        (<div className="text-[#FF3131] font-bold p-2 rounded-md ">
-                        {ticket.status}
-                        </div>):
-                     
-                      (<div className="text-[#4CBB17] font-bold p-2 rounded-md ">
-                      {ticket.status}
-                      </div>)}
-                     </td>
+                    <td>
+                      {ticket.status == "CLOSE" ? (
+                        <div className=" text-[#4CBB17] font-bold p-2 rounded-md ">
+                          {ticket.status}
+                        </div>
+                      ) : null}
+                      {ticket.status == "OPEN" ? (
+                        <div className=" text-[#FF3131] font-bold p-2 rounded-md ">
+                          {ticket.status}
+                        </div>
+                      ) : (
+                        <div className="text-[blue] font-bold p-2 rounded-md ">
+                          {ticket.status}
+                        </div>
+                      )}
+                    </td>
                   </tr>
                 );
               })}
             </tbody>
-            
           </table>
           {tickets.length !== 0 ? (
-          <div className="dashboard-content-footer">
-            {pagination.map((item, index) => (
-              <span
-                key={index}
-                className={item === page ? "active-pagination" : "pagination"}
-                onClick={() => __handleChangePage(item)}
-              >
-                {item}
-              </span>
-            ))}
-          </div>
-        ) : (
-          <div className="dashboard-content-footer">
-            <span className="empty-table">No data</span>
-          </div>
-        )}
+            <div className="dashboard-content-footer">
+              {pagination.map((item, index) => (
+                <span
+                  key={index}
+                  className={item === page ? "active-pagination" : "pagination"}
+                  onClick={() => __handleChangePage(item)}
+                >
+                  {item}
+                </span>
+              ))}
+            </div>
+          ) : (
+            <div className="dashboard-content-footer">
+              <span className="empty-table">No data</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
