974c6f3947466f37f0a3ffd2215446e4f92f9a01
Prasanna <prasannavelmurugan0200@gmail.com>
2023-01-31 13:26:25 +0530

    ev

--- controllers/User.js
+++ controllers/User.js
@@ -1,107 +1,114 @@
 const jwt = require("jsonwebtoken");
 const app = require("express");
-const { User } = require("../database/database");
-const { v4: uuidv4 } = require("uuid");
+const {User} = require("../database/database");
+const {v4: uuidv4} = require("uuid");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 exports.createAccount = async (req, res) => {
-  try {
-    const { user } = req.body;
+	try {
+		const {user} = req.body;
 
-    await User.create({
-      name: user.name,
-      password: user.password,
-      role: user.role,
-    }).then((response) => {
-      console.log(response);
-    });
-    // bcrypt.hash(user.password, saltRounds, async function (err, hash) {
-    //   if (err) return res.sendStatus(401);
+		await User.create({
+			name: user.name,
+			password: user.password,
+			role: user.role,
+		}).then((response) => {
+			console.log(response);
+		});
+		// bcrypt.hash(user.password, saltRounds, async function (err, hash) {
+		//   if (err) return res.sendStatus(401);
 
-    // })
-  } catch (error) {
-    console.log(error);
-  }
+		// })
+	} catch (error) {
+		console.log(error);
+	}
 };
 exports.showAccounts = async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.json(users);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const users = await User.findAll();
+		res.json(users);
+	} catch (error) {
+		console.log(error);
+	}
 };
 exports.updateAccount = async (req, res) => {
-  try {
-    const { user } = req.body;
-    return await User.update(user, { where: { id: user.id } });
-  } catch (error) {}
+	try {
+		const {user} = req.body;
+		console.log(user);
+		return await User.update(user, {where: {id: user.id}});
+	} catch (error) {
+		console.log(error);
+	}
 };
 exports.deleteAccount = async (req, res) => {
-  try {
-    return await User.destroy({ where: { id: req.params.id } });
-  } catch (error) {}
+	try {
+		return await User.destroy({where: {id: req.params.id}});
+	} catch (error) {}
 };
 exports.loginAccount = async (req, res) => {
-  try {
-    const { user } = req.body;
-    console.log(user, "user");
-    const users = await User.findOne({ where: { name: user.name } });
+	try {
+		const {user} = req.body;
+		console.log(user, "user");
+		const users = await User.findOne({where: {name: user.name}});
 
-    if (users?.dataValues.password == user.password) {
-      const accessToken = jwt.sign(
-        JSON.parse(JSON.stringify(users.dataValues)),
-        process.env.ACCESSTOKEN,
-        { expiresIn: "1d" }
-      );
-      const refreshToken = jwt.sign(
-        JSON.parse(JSON.stringify(users.dataValues)),
-        process.env.REQUESTTOKEN
-      );
+		if (users?.dataValues.password == user.password) {
+			const accessToken = jwt.sign(
+				JSON.parse(JSON.stringify(users.dataValues)),
+				process.env.ACCESSTOKEN,
+				{expiresIn: "1d"}
+			);
+			const refreshToken = jwt.sign(
+				JSON.parse(JSON.stringify(users.dataValues)),
+				process.env.REQUESTTOKEN
+			);
 
-      return res
-        .cookie("jwt", refreshToken, {
-          httpOnly: false,
-          sameSite: "None",
-          secure: true,
-          maxAge: 24 * 60 * 60 * 1000,
-        })
-        .json({ user: users, accessToken: accessToken, message: "success" });
-    } else {
-      console.log("====================================");
-      console.log("err");
-      console.log("====================================");
-      res.json({ message: "nouser" });
-    }
-  } catch (error) {
-    console.log(error);
-  }
+			return res
+				.cookie("jwt", refreshToken, {
+					httpOnly: false,
+					sameSite: "None",
+					secure: true,
+					maxAge: 24 * 60 * 60 * 1000,
+				})
+				.json({
+					user: users,
+					accessToken: accessToken,
+					message: "success",
+				});
+		} else {
+			console.log("====================================");
+			console.log("err");
+			console.log("====================================");
+			res.json({message: "nouser"});
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
 exports.refresh = async (req, res) => {
-  try {
-    console.log(cookies, "cookie");
-    const cookies = req.cookies;
-    console.log(req, "cookie");
-    if (!cookies?.jwt) return res.sendStatus(401);
-    const refreshToken = cookies.jwt;
-    jwt.verify(refreshToken, process.env.REQUESTTOKEN, (err, user) => {
-      if (err) return res.sendStatus(403);
-      const accessToken = jwt.sign(user, process.env.ACCESSTOKEN);
+	try {
+		console.log(cookies, "cookie");
+		const cookies = req.cookies;
+		console.log(req, "cookie");
+		if (!cookies?.jwt) return res.sendStatus(401);
+		const refreshToken = cookies.jwt;
+		jwt.verify(refreshToken, process.env.REQUESTTOKEN, (err, user) => {
+			if (err) return res.sendStatus(403);
+			const accessToken = jwt.sign(user, process.env.ACCESSTOKEN);
 
-      res.json({ accessToken: accessToken });
-    });
-  } catch (error) {
-    return res.sendStatus(403);
-  }
+			res.json({accessToken: accessToken});
+		});
+	} catch (error) {
+		return res.sendStatus(403);
+	}
 };
 
 exports.checkLogin = (req, res) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  console.log(token, "tok");
-  jwt.verify(token, process.env.ACCESSTOKEN, (err, user) => {
-    if (err) res.sendStatus(401);
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
+	console.log(token, "tok");
+	jwt.verify(token, process.env.ACCESSTOKEN, (err, user) => {
+		if (err) res.sendStatus(401);
 
-    res.sendStatus(200);
-  });
+		res.sendStatus(200);
+	});
 };
