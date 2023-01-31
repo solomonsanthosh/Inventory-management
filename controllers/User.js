b56c56fda04c2c806142c8a9300ccb175e9626fe
solomonsanthosh <solosanty2064@gmail.com>
2023-01-31 11:27:56 +0530

    SS

--- controllers/User.js
+++ controllers/User.js
@@ -7,11 +7,11 @@ const saltRounds = 10;
 exports.createAccount = async (req, res) => {
   try {
     const { user } = req.body;
-    
+
     await User.create({
       name: user.name,
       password: user.password,
-      role:user.role
+      role: user.role,
     }).then((response) => {
       console.log(response);
     });
@@ -35,28 +35,33 @@ exports.showAccounts = async (req, res) => {
 exports.loginAccount = async (req, res) => {
   try {
     const { user } = req.body;
-    console.log(user,'user');
+    console.log(user, "user");
     const users = await User.findOne({ where: { name: user.name } });
 
     if (users?.dataValues.password == user.password) {
       const accessToken = jwt.sign(
         JSON.parse(JSON.stringify(users.dataValues)),
-        process.env.ACCESSTOKEN,{expiresIn:'1d'}
+        process.env.ACCESSTOKEN,
+        { expiresIn: "1d" }
       );
       const refreshToken = jwt.sign(
         JSON.parse(JSON.stringify(users.dataValues)),
         process.env.REQUESTTOKEN
       );
-      
-      return res.cookie('jwt', refreshToken, { httpOnly: false, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 }).json({ user:users, accessToken: accessToken , message:"success" });
-      
 
-    } else
-    {
-      console.log('====================================');
+      return res
+        .cookie("jwt", refreshToken, {
+          httpOnly: false,
+          sameSite: "None",
+          secure: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .json({ user: users, accessToken: accessToken, message: "success" });
+    } else {
+      console.log("====================================");
       console.log("err");
-      console.log('====================================');
-      res.json({message:"nouser"})
+      console.log("====================================");
+      res.json({ message: "nouser" });
     }
   } catch (error) {
     console.log(error);
@@ -64,9 +69,9 @@ exports.loginAccount = async (req, res) => {
 };
 exports.refresh = async (req, res) => {
   try {
-    console.log(cookies,'cookie');
+    console.log(cookies, "cookie");
     const cookies = req.cookies;
-    console.log(req,'cookie');
+    console.log(req, "cookie");
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
     jwt.verify(refreshToken, process.env.REQUESTTOKEN, (err, user) => {
@@ -75,24 +80,18 @@ exports.refresh = async (req, res) => {
 
       res.json({ accessToken: accessToken });
     });
-
-    
   } catch (error) {
     return res.sendStatus(403);
   }
 };
 
-
-
-exports.checkLogin = (req,res) => {
-  const authHeader = req.headers['authorization'];
+exports.checkLogin = (req, res) => {
+  const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  console.log(token,'tok');
-  jwt.verify(token,process.env.ACCESSTOKEN,(err,user)=>{
-      if(err) res.sendStatus(401)
-      
-      res.sendStatus(200)
-      
-  })
+  console.log(token, "tok");
+  jwt.verify(token, process.env.ACCESSTOKEN, (err, user) => {
+    if (err) res.sendStatus(401);
 
-}     
+    res.sendStatus(200);
+  });
+};
