020b2f0b47bf7beb20889bc311514ae09f111645
Prasanna <prasannavelmurugan0200@gmail.com>
2023-02-01 18:53:45 +0530

    rv

--- src/Axios/manager.js
+++ src/Axios/manager.js
@@ -1,34 +1,40 @@
 import axios from "axios";
+const server_link = "https://inventory-server-3jgi.onrender.com";
 
 export const createAccountAxios = async (user) => {
 	console.log("user", user);
-	return await axios.post(`http://localhost:8080/create`, {
+	return await axios.post(`${server_link}/create`, {
 		user: user,
 	});
 };
 
 export const getAccounts = async () => {
-	return await axios.get(`${process.env.REACT_APP_BASE_URL}/show`);
+	// return await axios.get(`${process.env.REACT_APP_BASE_URL}/show`);
+	return await axios.get(`${server_link}/show`);
 };
 
 export const updateAccount = async (user) => {
-	return await axios.put(`${process.env.REACT_APP_BASE_URL}/update`, {
+	// return await axios.put(`${process.env.REACT_APP_BASE_URL}/update`, {
+	return await axios.put(`${server_link}/update`, {
 		user: user,
 	});
 };
 
 export const deleteAccount = async (id) => {
-	return await axios.delete(`${process.env.REACT_APP_BASE_URL}/delete/${id}`);
+	// return await axios.delete(`${process.env.REACT_APP_BASE_URL}/delete/${id}`);
+	return await axios.delete(`${server_link}/delete/${id}`);
 };
 
 export const getSuggestion = async (partno) => {
 	return await axios.get(
-		`${process.env.REACT_APP_BASE_URL}/getsuggestions/${partno}`
+		// `${process.env.REACT_APP_BASE_URL}/getsuggestions/${partno}`
+		`${server_link}/getsuggestions/${partno}`
 	);
 };
 
 export const sendEmail = async (id, product, quantity) => {
-	return await axios.post(`${process.env.REACT_APP_BASE_URL}/sendemail`, {
+	// return await axios.post(`${process.env.REACT_APP_BASE_URL}/sendemail`, {
+	return await axios.post(`${server_link}/sendemail`, {
 		company_id: id,
 		product: product,
 		quantity: quantity,
--- src/Axios/store.js
+++ src/Axios/store.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+const server_link = "https://inventory-server-3jgi.onrender.com";
+
 export const getProducts = async () => {
-	return await axios.get(`${process.env.REACT_APP_BASE_URL}/allproducts/`);
+	// return await axios.get(`${process.env.REACT_APP_BASE_URL}/allproducts/`);
+	return await axios.get(`${server_link}/allproducts/`);
 };
 
 export const postStore = async (product_id) => {
-	return await axios.get(`${process.env.REACT_APP_BASE_URL}/postStore/${product_id}`);
+	// return await axios.get(`${process.env.REACT_APP_BASE_URL}/postStore/${product_id}`);
+	return await axios.get(`${server_link}/postStore/${product_id}`);
 };
